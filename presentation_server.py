65e667afa46b35684bcf30c463604eb02da7ee22
sebastien nadeau <snadeau@breakingwalls.co>
2017-11-28 01:01:56 -0500

    added input for presentation slide number

--- presentation_server.py
+++ presentation_server.py
@@ -25,6 +25,26 @@ except ImportError:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
+bShouldExit = False
+slideNumber = 0
+numberConnections = 0
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
 
 def initui():
     os.system('clear')
@@ -41,8 +61,10 @@ def initui():
     sw.printStatic("q : Exit ", 0, 11)
 
     #test layour
-    sw.printStatic("Table  : ", 42, 13)
-    sw.printStatic("BetPos : ", 42, 14)
+    sw.printStatic("current slide : ", 42, 13)
+    sw.printStatic("set slide to : ", 42, 14)
+
+    sw.printStatic("Active Connections : ", 42, 16, 1)
 
 
 
@@ -84,8 +106,8 @@ def startserver():
 
 
     # Start window
-    sw.printStatic("STARTING", 51, 13, 1)
-    sw.printStatic("ACTIVE  ", 51, 13, 3)
+    #sw.printStatic("STARTING", 51, 13, 1)
+    #sw.printStatic("ACTIVE  ", 51, 13, 3)
 
 
 
@@ -101,7 +123,18 @@ def startserver():
     '''
 
     #listen for key input
-    start_new_thread(keylistener ,(s,))
+    start_new_thread(keylistener, (s,))
+
+    start_new_thread(listen, (s, ))
+
+    # now saty awake until user quits
+    while(not bShouldExit):
+        time.sleep(5)
+        sw.printStatic(numberConnections, 63, 16, 1)
+
+    sw.printnl('shutdown complete', 5)
+
+def listen(s):
 
     while 1:
         #wait to accept a connection - blocking call
@@ -113,20 +146,40 @@ def startserver():
         sw.printnl('Connected with ' + addr[0] + ':' + str(addr[1]))
 
         #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+
+
         start_new_thread(clientthread ,(conn,rw,rg))
 
     #s.close()
 
 def keylistener(s):
 
+    global bShouldExit
+    global slideNumber
+
+    inputNumber = ''
     while 1:
         keyinput = getch()
         if keyinput == 'q':
-            s.shutdown(socket.SHUT_RDWR)
+            #s.shutdown(socket.SHUT_RDWR)
             s.close()
             sw.printnl('server closing connection', 4)
+            bShouldExit = True
             break
-        #print keyinput
+        elif keyinput == '\r':
+            if inputNumber != '':
+                slideNumber = inputNumber
+                sw.printStatic(slideNumber+'     ', 58, 13, 3)
+                inputNumber = ''
+                sw.printStatic('        ', 57, 14, 4)
+        elif keyinput == '\x08':  # handle backspace
+            inputNumber = inputNumber[:-1]
+            sw.printStatic(inputNumber+'     ', 57, 14, 4)
+        else:
+            if is_number(keyinput):
+                inputNumber += keyinput
+                sw.printStatic(inputNumber, 57, 14, 4)
+
 
 
 #Function for handling connections. This will be used to create threads
@@ -191,4 +244,5 @@ if __name__=='__main__':
     sw.printStatic('press Enter to Continue...', 0, 11, 5)
     raw_input("")
     os.system('clear')
+    os.system('cls')
 
